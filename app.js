5a62671b8b8f1a2ecd339a0c9ef1276cad5e807f
mmaquer2 <mike.maquera212@gmail.com>
2019-09-28 16:08:01 -0500

    adding api data pulls

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-// sources for of APIs
-    //-Api news (headlines)
-    //alpha vantage (stocks)
-    //espn (sports)
-    //wolfram alpha api
+
     function test() {
         console.log('hello world')
         alert('test is working')
@@ -10,15 +6,23 @@
         document.getElementById('search-out').innerHTML = "test test test"
     }
 
-    const alpha_key = ('YIF9NU7FLVJWG46Q');
-    const url = ('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=MSFT&apikey=YIF9NU7FLVJWG46Q');
-    async function test_data() {
+    //const watchlist = []
+    //const alpha_key = ('YIF9NU7FLVJWG46Q');
+    const url = ('https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=MSFT&apikey=YIF9NU7FLVJWG46Q');
+    async function stock_api() {
 
-        const response =await fetch(url);
-        const data = await response.json();
+        const response = await fetch(url);
+        const stock_data = await response.json();  
+        console.log(stock_data)
 
-
-        console.log(data)
+        var i = 0;
+        var price = ''
+      /*  for(i;i<stock_data.length;i++){
+            price = stock_data.high[i]
+            console.log(price)
+        }
+*/
+      alert('hello world')
 
     }
 
@@ -32,13 +36,27 @@
 
     const news_key = ('a0797ceb9fc0435c9e7e691760ad4244')
     const news_url = ('https://newsapi.org/v2/top-headlines?country=us&apiKey=a0797ceb9fc0435c9e7e691760ad4244')
-    async function test_news_api () {
+    async function test_news_api() {
 
         const response = await fetch(news_url);
         const news_data = await response.json();
+        
+           // console.log (news_data.articles[0].title,news_data.articles[0].description,news_data.articles[0].publishedAt,news_data.articles[0].source.name)
+        var i = 0;
+        var titles = ""
+        var desc = ""
+        for(i; i<news_data.articles.length;i++){
+                titles = news_data.articles[i].title;
+                desc = news_data.articles[i].description;
+                console.log(titles,desc)
+            
+            }
+                
 
-        console.log (news_data)
-    }
+        }
+
+
+    
 
 
 
@@ -73,6 +91,55 @@
     }
 
     */
+
+    async function weather() {
+
+        const start = ('https://api.openweathermap.org/data/2.5/weather?q=')
+        const id =('&APPID=a488d31fefafdc561500bdfd1b695f5d')
+        const url =(start + "nashville" + id)
+      
+        const response = await fetch(url);
+        const data = await response.json();
+        var string = "";
+        var k_temp = data.main.temp;
+        var desc = data.weather[0].description;
+        
+        //convert kelvin to degrees F
+        f_temp = ((k_temp - 273.15) * 1.8 + 32).toFixed(1);
+        
+        string ='<div class="card"> <div><span class=""> City: '+data.name+'</span></div><div class=""></div><div class="col-xs-5"> Currently is: '+f_temp+' F </div><div>'+data.main.humidity+' % Humidity </div> <div> '+data.weather[0].description+'</div> </div><br></br>';
+        console.log(data.weather[0].main)
+        //console.log(data.main.temp)
+        //  console.log(data.main.humidity)
+        //  console.log(data.weather[0].main)
+         console.log(data.weather[0].description)
+         console.log(data.weather[0].icon)
+        // console.log(data.name)
+    
+     document.getElementById('weather_output').innerHTML =string  
+     VisualWeather_test(k_temp,desc) 
+
+    }
+
+
+    function VisualWeather_test (temp,des) {
+    
+        if (temp < 300) {
+        document.getElementById('weather-icon').className = "fas fa-cloud fa-10x"
+        document.getElementById('weather').style.backgroundColor = "lightgrey";
+        
+        }
+        else if ( temp > 300) {
+            document.getElementById('weather').style.backgroundColor = "skyblue";
+            document.getElementById('weather-icon').className = "fas fa-sun fa-10x"
+        }
+        
+        else {
+            document.getElementById('weather-icon').className = "fas fa-cloud fa-10x"
+            document.getElementById('weather').style.backgroundColor = "grey";
+        }
+        
+     }
 
     //refreshs page with FA icon
 
