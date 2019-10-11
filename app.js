b619be5c1cfb44493091320e239fbfa2a1f44ebb
mmaquer2 <mike.maquera212@gmail.com>
2019-10-11 17:11:12 -0500

    traffic map added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ window.onload = function(){
   this.time();
 // this.test();
  this.weather();
+ this.initMap();
   // this.stock_api();
    this.news_api();
    this.stock_news();
@@ -228,6 +229,18 @@ var today = dd + '/' + mm + '/' + yyyy;
             }
             
          }
+
+
+
+         function initMap() {
+            var map = new google.maps.Map(document.getElementById('map'), {
+              zoom: 13,
+              center: {lat: 34.04924594193164, lng: -118.24104309082031}
+            });
+    
+            var trafficLayer = new google.maps.TrafficLayer();
+            trafficLayer.setMap(map);
+          }
 
 
 
